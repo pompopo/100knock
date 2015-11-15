260b3114dbe6108292cc143df2d16532a643cb1c
pompopo <pompopo@gmail.com>
2015-11-16 00:23:08 +0900

    refactor 26

--- chapter03/26.exs
+++ chapter03/26.exs
@@ -1,16 +1,18 @@
 require Problem25
 defmodule Problem26 do
   def solve do
-    dict = Problem25.solve
-
-    Dict.keys(dict) |> Enum.reduce(%{}, fn(key, acc) ->
-                                         new_value = strip_markup(Dict.get(dict, key))
-                                         Dict.put(acc, key, new_value)
-                                       end)
+    regexs = [~r/'''(.*)'''/, ~r/''(.*)''/, ~r/'(.*)'/]
+    apply_regexs(Problem25.solve, regexs)
   end
 
-  defp strip_markup(str) do
-    regexs = [~r/'''(.*)'''/, ~r/''(.*)''/, ~r/'(.*)'/]
+  def apply_regexs(dict, regexs) do
+    Dict.keys(dict) |> Enum.reduce(%{}, fn(key, acc) ->
+                                          new_value = convert(regexs, Dict.get(dict, key))
+                                          Dict.put(acc, key, new_value)
+                                        end)
+  end
+
+  defp convert(regexs, str) do
     Enum.reduce(regexs, str, fn(regex, acc) -> Regex.replace(regex, acc, "\\1") end)
   end
 end
